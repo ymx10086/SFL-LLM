b9daedf71ad1a8e9d1b126efaab5996721d24aa6
“stupidtree” <2321182768@qq.com>
2024-06-15 22:28:53 +0800

    + readme

--- experiments/scripts/pipeline/demo_bisr.sh
+++ experiments/scripts/pipeline/demo_bisr.sh
@@ -1,72 +1,82 @@
-# 实验：对Embedding Inversion Attack进行超参搜索
 seed=42
-
+sps="6-27"
+batch_size=2
+attacker_freq=200
+attacker_samples=5
+max_global_step=605
 dataset_label='train'
-exp_name='[EXP]BiSR(b+f)_cross_model'
 global_round=1
 client_steps=500
 noise_scale=0.0
 noise_mode="none"
 data_shrink_frac=0.08
-test_data_shrink_frac=0.1
-evaluate_freq=100
+test_data_shrink_frac=0.3
+evaluate_freq=300
 self_pt_enable=False
 lora_at_trunk=True
 lora_at_bottom=True
 lora_at_top=True
-
-sps="6-27"
-batch_size=2
-
-attacker_freq=200
-attacker_samples=5
-max_global_step=605
-
 collect_all_layers=True
 
-sfl_datasets=("piqa")
-sfl_model_name='codegen'
 
+model_names=('llama2')
 sip_inverter_dataset='sensireplaced'
-sip_inverter_models=('llama2')
-sip_layer=6
+sfl_datasets=("gsm8k" "wikitext" "piqa" "codealpaca" "dialogsum" "sensimarked")
+exp_name='[EXP]BiSR'
 
-gma_lr=0.09
-gma_beta=0.85
-gma_epc=18
-gma_init_temp=1.2
-gsma_lr=0.005
-gsma_epc=800
-gsma_wd=0.02
-gsma_cross_model='llama2'
-load_bits=32
-# 0.05 0.001 0.1)
+for model_name in "${model_names[@]}"; do
+  for sfl_dataset in "${sfl_datasets[@]}"; do
+    case_name="BiSR(b+f)@${model_name}@${sfl_dataset}"
 
-for sfl_dataset in "${sfl_datasets[@]}"; do
-  for sip_inverter_model in "${sip_inverter_models[@]}"; do
+    if [ "$model_name" == "llama2" ]; then
+      gma_lr=0.09
+      gma_beta=0.85
+      gma_epc=18
+      gma_init_temp=1.2
+      gsma_lr=0.005
+      gsma_epc=800
+      gsma_wd=0.02
+    fi
 
-    case_name="CM-${sfl_model_name}-${sfl_dataset}<<-${sip_inverter_model}-${sip_inverter_dataset}-l${sip_layer}-SCM${sma_cross_model}"
+    if [ "$model_name" == "gpt2-large" ]; then
+      gma_lr=0.09
+      gma_beta=0.85
+      gma_epc=32
+      gma_init_temp=1.2
+      gsma_lr=0.01
+      gsma_epc=800
+      gsma_wd=0.01
+    fi
 
-    # 先训练攻击模型
+    if [ "$model_name" == "chatglm" ]; then
+      gma_lr=0.09
+      gma_beta=0.85
+      gma_epc=18
+      gma_init_temp=1.2
+      gsma_lr=0.005
+      gsma_epc=800
+      gsma_wd=0.01
+    fi
+
+    # Tran the SIP inverter first
     echo "Running train_inverter.py"
     python ../py/train_inverter.py \
-      --model_name "$sip_inverter_model" \
+      --model_name "$model_name" \
       --seed "$seed" \
       --attack_model "gru" \
       --dataset "$sip_inverter_dataset" \
       --attack_mode 'b2tr' \
-      --sps "${sip_layer}-22" \
+      --sps "$sps" \
       --dataset_test_frac 0.1 \
       --save_checkpoint True \
-      --log_to_wandb False\
-      --load_bits "$load_bits"
+      --log_to_wandb False
 
-    # 将其用于攻击
+    # Run the simulation
     echo "Running evaluate_tag_methods.py with sfl_ds=$sfl_dataset"
     python ../py/sim_with_attacker.py \
       --noise_mode "$noise_mode" \
       --case_name "$case_name" \
-      --model_name "$sfl_model_name" \
+      --model_name "$model_name" \
       --split_points "$sps" \
       --global_round "$global_round" \
       --seed "$seed" \
@@ -85,29 +95,25 @@ for sfl_dataset in "${sfl_datasets[@]}"; do
       --collect_all_layers "$collect_all_layers" \
       --dataset_label "$dataset_label" \
       --batch_size "$batch_size" \
-      --attacker_freq "$attacker_freq" \
-      --attacker_samples "$attacker_samples" \
-      --max_global_step "$max_global_step" \
-      --sip_dataset "$sip_inverter_dataset" \
       --tag_enable False \
       --gma_enable True \
       --gsma_enable True \
       --sma_enable False \
       --eia_enable False \
+      --attacker_freq "$attacker_freq" \
+      --attacker_samples "$attacker_samples" \
+      --max_global_step "$max_global_step" \
+      --sip_dataset "$sip_inverter_dataset" \
       --sip_prefix "normal" \
       --sip_b2tr_enable True \
-      --sip_b2tr_layer "$sip_layer" \
+      --sip_b2tr_layer -1 \
       --sip_tr2t_enable False \
-      --sip_attack_all_layers False \
-      --sip_target_model_name "$sip_inverter_model" \
+      --gma_lr "$gma_lr" \
       --gma_beta "$gma_beta" \
       --gma_epochs "$gma_epc" \
       --gma_init_temp "$gma_init_temp" \
-      --gma_lr "$gma_lr" \
       --gsma_lr "$gsma_lr" \
       --gsma_epochs "$gsma_epc" \
-      --gsma_wd "$gsma_wd" \
-      --gsma_cross_model "$gsma_cross_model"\
-      --load_bits "$load_bits"
+      --gsma_wd "$gsma_wd"
   done
 done
